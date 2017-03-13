c54efe494c414aedb75bc4806cf5778d4d773ceb
RickSeymour <email@rickseymour.com>
2017-03-13 21:43:42 +0000

    Apply fixes from StyleCI

--- tests/_testPlates.php
+++ tests/_testPlates.php
@@ -1,5 +1,7 @@
 <?php
+
 use BespokeSupport\Reg\Reg;
+
 return [
     null => [
         'registration' => null,
@@ -12,138 +14,138 @@ return [
     ],
     'A1' => [
         'registration' => 'A 1',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'A11' => [
         'registration' => 'A 11',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'A111' => [
         'registration' => 'A 111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'A1111' => [
         'registration' => 'A 1111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AA1' => [
         'registration' => 'AA 1',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AA11' => [
         'registration' => 'AA 11',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AA111' => [
         'registration' => 'AA 111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AA1111' => [
         'registration' => 'AA 1111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AAA1' => [
         'registration' => 'AAA 1',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AAA11' => [
         'registration' => 'AAA 11',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AAA111' => [
         'registration' => 'AAA 111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'AAA1111' => [
         'registration' => 'AAA 1111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'IAA1111' => [
         'registration' => 'IAA 1111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     'ZAA1111' => [
         'registration' => 'ZAA 1111',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     '1A' => [
         'registration' => '1 A',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '11A' => [
         'registration' => '11 A',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '111A' => [
         'registration' => '111 A',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '1111A' => [
         'registration' => '1111 A',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '1AA' => [
         'registration' => '1 AA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '11AA' => [
         'registration' => '11 AA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '111AA' => [
         'registration' => '111 AA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '1111AA' => [
         'registration' => '1111 AA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '1AAA' => [
         'registration' => '1 AAA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '11AAA' => [
         'registration' => '11 AAA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '111AAA' => [
         'registration' => '111 AAA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     '1111AAA' => [
         'registration' => '1111 AAA',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     'A1AAA' => [
         'registration' => 'A1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'A11AAA' => [
         'registration' => 'A11 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'A111AAA' => [
         'registration' => 'A111 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'AA11AAA' => [
         'registration' => 'AA11 AAA',
-        'style' => Reg::STYLE_CURRENT
+        'style'        => Reg::STYLE_CURRENT,
     ],
     'AAA1A' => [
         'registration' => 'AAA 1A',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'AAA11A' => [
         'registration' => 'AAA 11A',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'AAA11AA' => [
         'registration' => null,
     ],
     'AAA111A' => [
         'registration' => 'AAA 111A',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'III' => [
         'registration' => null,
@@ -159,11 +161,11 @@ return [
     ],
     'ZZZ1' => [
         'registration' => 'ZZZ 1',
-        'style' => Reg::STYLE_DATELESS_LETTER
+        'style'        => Reg::STYLE_DATELESS_LETTER,
     ],
     '1ZZZ' => [
         'registration' => '1 ZZZ',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     'QQQ' => [
         'registration' => null,
@@ -180,7 +182,7 @@ return [
     ],
     'A111QAA' => [
         'registration' => 'A111 QAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'A111ZAA' => [
         'registration' => null,
@@ -199,43 +201,43 @@ return [
     ],
     'I1AAA' => [
         'registration' => 'I1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'O1AAA' => [
         'registration' => 'O1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'Q1AAA' => [
         'registration' => 'Q1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'U1AAA' => [
         'registration' => 'U1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'Z1AAA' => [
         'registration' => 'Z1 AAA',
-        'style' => Reg::STYLE_PREFIX
+        'style'        => Reg::STYLE_PREFIX,
     ],
     'AAA111I' => [
         'registration' => 'AAA 111I',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'AAA111O' => [
         'registration' => 'AAA 111O',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'AAA111Q' => [
         'registration' => 'AAA 111Q',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     'AAA111Z' => [
         'registration' => 'AAA 111Z',
-        'style' => Reg::STYLE_SUFFIX
+        'style'        => Reg::STYLE_SUFFIX,
     ],
     '11Q' => [
         'registration' => '11 Q',
-        'style' => Reg::STYLE_DATELESS_NUMBER
+        'style'        => Reg::STYLE_DATELESS_NUMBER,
     ],
     'A' => [
         'registration' => null,
